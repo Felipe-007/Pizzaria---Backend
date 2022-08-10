5cc52f268ec9393d90947ad3f43e3082eca5fb4b
Felipe <felipe.presidencia@gmail.com>
2022-08-10 12:53:57 -0300

    C1 - Tratando erros

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,23 @@
 import express, { Request, Response, NextFunction } from "express";
+import 'express-async-errors';
 import { router } from "./routes";
+import cors from 'cors';  //habita a qualquer IP a fazer requisição
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
+
+//Tratando as mensagens de Erro
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    //Se for uma instância do tipo error
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+})
 
 app.listen(3333, () => {
   console.log('🚀 Back-end Inicializado!');
