28eb3c39a86543cdb32159a1bf0b79c5be855675
Felipe <felipe.presidencia@gmail.com>
2022-08-11 22:01:33 -0300

    C3 - Criando models, Criando migration

--- notas.js
+++ notas.js
@@ -5,4 +5,7 @@
  * yarn add prisma
  * yarn add @prisma/client
  * npx prisma init
+ * 
+ * yarn prisma migrate dev
+ * Enter a name for the new migration: » create-table-users  OU create-models-pizzaria
  */
